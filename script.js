0bc76a28443873d10a58d2a61dd97098a0684d45
Victor Krieger Almeida <105669074+KriegerVictor@users.noreply.github.com>
2022-05-19 09:36:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector (".linemenu"). addEventListener ("click", ()=>                       // Animação da sidebar
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+ 
